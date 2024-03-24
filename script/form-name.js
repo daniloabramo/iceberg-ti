a6d5d351b8a2d88e39d9c01575a0ce44fb96e6d4
daniloabramo <142537432+daniloabramo@users.noreply.github.com>
2024-03-23 22:36:20 -0300

    feat(form): introduction form

---
+++ script/form-name.js
@@ -0,0 +1,6 @@
+document.getElementById('preencherCampos').addEventListener('click', function() {
+    document.getElementById('email').value = 'exemplo@email.com';
+    document.getElementById('senha').value = 'senha123';
+   });
+   
+   
