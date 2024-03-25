f2f10c0f69711dca00cda77f6628cb31ccfd2cd2
daniloabramo <142537432+daniloabramo@users.noreply.github.com>
2024-03-25 00:33:29 -0300

    feat(ui) article layout redesign

--- script/form-name.js
+++ script/form-name.js
@@ -1,6 +1,23 @@
-document.getElementById('preencherCampos').addEventListener('click', function() {
-    document.getElementById('email').value = 'exemplo@email.com';
-    document.getElementById('senha').value = 'senha123';
-   });
+const emails = ["exemplo1@email.com", "exemplo2@email.com", "exemplo3@email.com"];
+const senhas = ["senha123", "senha456", "senha789"];
+
+function selecionarAleatorio(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+document.getElementById('autoComplete').addEventListener('click', function() {
+    let email = selecionarAleatorio(emails);
+    let senha = selecionarAleatorio(senhas);
+
+    // Modificar o texto dos elementos <p> existentes
+    document.getElementById('email').innerText = email;
+    document.getElementById('senha').innerText = senha;
+
+    // Agora você pode usar as variáveis email e senha conforme necessário
+    console.log(email, senha);
+});
+
+
+
    
-   
+
